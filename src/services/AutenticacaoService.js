1f19b751e02cd61781ceaf976ae95df4ef399af8
Kenzo Sato <kenzo_sato@estudante.sc.senai.br>
2023-03-30 21:32:32 -0300

    commit

--- src/services/AutenticacaoService.js
+++ src/services/AutenticacaoService.js
@@ -1,19 +1,11 @@
 import axios from "axios";
 
-const url = "https://localhost:8443/editoralivros/login";
-
-/*
-
-*/
+const url = "http://localhost:8443/login";
 
 class AutenticacaoService {
   login(user) {
     const config = {
       withCredentials: true,
-      headers: {
-        "content-type": "application/json",
-        "access-control-allow-origin": "*",
-      },
     };
 
     return axios.post(url + "/auth", user, config);
